711ecf3684e66807aa7840245b18ca9d71fb6ea1
SamyrOR <saamyr@live.com>
2021-07-09 18:08:16 -0300

    Started the game info project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,45 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+
+import { MatTabsModule } from '@angular/material/tabs';
+import { MatIconModule } from '@angular/material/icon';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatSelectModule } from '@angular/material/select';
+import { MatButtonModule } from '@angular/material/button';
+
+import { GaugeModule } from 'angular-gauge';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SearchBarComponent } from './components/search-bar/search-bar.component';
+import { HomeComponent } from './components/home/home.component';
+
+const MAT_MODULES = [
+  MatTabsModule,
+  MatIconModule,
+  MatFormFieldModule,
+  MatSelectModule,
+];
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent, SearchBarComponent, HomeComponent],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    FormsModule,
+    GaugeModule,
+    MatTabsModule,
+    MatIconModule,
+    MatFormFieldModule,
+    MatSelectModule,
+    MatButtonModule,
+    HttpClientModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
---
+++ src/app/models.ts
@@ -0,0 +1,45 @@
+export interface Game {
+  background_image: string;
+  name: string;
+  released: string;
+  metacritic_url: string;
+  website: string;
+  description: string;
+  metacritic: string;
+  genres: Array<Genre>;
+  parent_platforms: Array<ParentPlatforms>;
+  publishersL: Array<Publishers>;
+  ratings: Array<Rating>;
+  screenshots: Array<Screenshots>;
+  trailers: Array<Trailer>;
+}
+
+export interface APIResponse<T> {
+  results: Array<T>;
+}
+
+interface Genre {
+  name: string;
+}
+
+interface ParentPlatforms {
+  platform: { name: string };
+}
+
+interface Publishers {
+  name: string;
+}
+
+interface Rating {
+  id: number;
+  count: number;
+  title: string;
+}
+
+interface Screenshots {
+  image: string;
+}
+
+interface Trailer {
+  data: { max: string };
+}
